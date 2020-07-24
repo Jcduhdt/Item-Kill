d597fec9383a3123b41744aa1c07dd229091eaea
Jcduhdt <15150651313@126.com>
2020-07-24 14:25:22 +0800

    增加雪花算法生成分布式订单唯一ID的注释

--- server/src/main/java/com/zx/server/utils/SnowFlake.java
+++ server/src/main/java/com/zx/server/utils/SnowFlake.java
@@ -6,6 +6,19 @@ package com.zx.server.utils;
  * @date 2020-07-11
  * 雪花算法 生成分布式唯一ID
  * 整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞，高效率
+ * Twitter_Snowflake
+ * SnowFlake的结构如下(每部分用-分开):
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ *  1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
+ *  41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+ *      得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，（如下下面程序IdWorker类的startTime属性）。
+ *      41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+ *  10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
+ *  12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
+ * 加起来刚好64位，为一个Long型。
+ * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，
+ * 并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
+ * https://blog.csdn.net/xiaopeng9275/article/details/72123709
  */
 public class SnowFlake {
 
@@ -23,6 +36,9 @@ public class SnowFlake {
 
     /**
      * 每一部分的最大值
+     * MAX_DATA_CENTER_NUM 最大的数据中心数 0~31
+     * MAX_MACHINE_NUM 机器序列数0~31
+     * MAX_SEQUENCE 最大的序列号数0~4095
      */
     private final static long MAX_DATA_CENTER_NUM = -1L ^ (-1L << DATA_CENTER_BIT);
     private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
@@ -30,6 +46,10 @@ public class SnowFlake {
 
     /**
      * 每一部分向左的位移
+     * 最右是12个序列位
+     * 机器数左移12位
+     * 数据中心数左移5+12位
+     * 时间戳数左移5+5+12位
      */
     private final static long MACHINE_LEFT = SEQUENCE_BIT;
     private final static long DATA_CENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
@@ -82,6 +102,11 @@ public class SnowFlake {
                 | sequence;                             //序列号部分
     }
 
+    /**
+     * 当前ms的4093个时间序列已经用尽
+     * 获取下一ms
+     * @return
+     */
     private long getNextMill() {
         long mill = getNewStamp();
         while (mill <= lastStamp) {
@@ -90,6 +115,10 @@ public class SnowFlake {
         return mill;
     }
 
+    /**
+     * 获取当前时间戳
+     * @return
+     */
     private long getNewStamp() {
         return System.currentTimeMillis();
     }
--- server/src/test/java/Test.java
+++ server/src/test/java/Test.java
@@ -8,5 +8,9 @@ import java.util.Date;
 public class Test {
     public static void main(String[] args) {
         System.out.println(new Date());
+        System.out.println(-1L ^ (-1L << 12));
+        System.out.println(Long.toHexString(-1L));
+        System.out.println(Long.toHexString((-1L << 12)));
+        System.out.println(Long.toHexString(System.currentTimeMillis()));
     }
 }
